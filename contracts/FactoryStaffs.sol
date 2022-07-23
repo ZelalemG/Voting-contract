e9be2b4bb6748391503e9e00f43644b22a5f42a3
ZelalemGG <midzele@gmail.com>
2022-07-23 11:12:55 +0300

    First push to remote repo

---
+++ contracts/FactoryStaffs.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.4; 
+
+import '@openzeppelin/contracts/access/Ownable.sol';
+import "@optionality.io/clone-factory/contracts/CloneFactory.sol";  
+
+contract FactoryStaffs {
+
+}
